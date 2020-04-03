93f7458278dee2828e5f3578d4ef5e52a15a36a5
penta <penta@192.168.1.6>
2020-04-03 17:21:25 +0700

    Đặng Việt Trung từ từ

---
+++ cong2snt/src/cong2snt/test.java
@@ -0,0 +1,19 @@
+package cong2snt;
+import java.util.*;
+public class test {
+
+	public static void main(String[] args) {
+		 int a, b, kq;	        
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Hello "); 
+	        System.out.print("Nhap a: "); 
+	        a = sc.nextInt(); 
+	        System.out.print("Nhap b: ");
+	        b = sc.nextInt();
+	      
+	        kq=a+b;
+	        
+	        System.out.println("Ket Qua:" + kq);
+	}
+
+}
