a48320cdac510c7e6c1a177a8534026db92f17c4
trungphamcong <63086760+trungphamcong@users.noreply.github.com>
2020-04-03 20:50:30 +0700

    Update phep_tru.java
    đã sửa

--- maytinh/src/maytinh/phep_tru.java
+++ maytinh/src/maytinh/phep_tru.java
@@ -2,22 +2,29 @@ package maytinh;
 import java.util.Scanner;
 
 public class phep_tru {
-
+	public int aNumber, bNumber;
+		
+	int pheptru() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Nhập vào số thứ nhất: ");
+		aNumber = scanner.nextInt();
+		System.out.println("Nhập vào số thứ hai: ");
+		bNumber = scanner.nextInt();
+		System.out.println("Nhập phép toán (-): ");
+		return aNumber - bNumber;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int aNumber, bNumber;
 		
-        Scanner scanner = new Scanner(System.in);
+        phep_tru p = new phep_tru();
+        System.out.println(p.pheptru());
      
-        System.out.println("Nhập vào số thứ nhất: ");
-        aNumber = scanner.nextInt();
-        System.out.println("Nhập vào số thứ hai: ");
-        bNumber = scanner.nextInt();
-        System.out.println("Nhập phép toán (-): ");
+       
 
 
-            System.out.println(aNumber + " - " + bNumber + " = " + (aNumber - bNumber));
+            
 
 	}
-
 }
+
